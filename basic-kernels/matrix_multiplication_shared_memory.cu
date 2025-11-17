1270d83f4aab5f70e993f4df6281222247d76ce9
kondrauer <maximilian.schlosser@yahoo.de>
2025-11-17 18:02:37 +0100

    matmul2d shared memory optimization
    Signed-off-by: kondrauer <maximilian.schlosser@yahoo.de>

---
+++ basic-kernels/matrix_multiplication_shared_memory.cu
@@ -0,0 +1,90 @@
+#include <cstdio>
+
+#define TILE_SIZE 16
+
+__global__ void MatMul2D(float* A, float* B, float* C, int M, int N, int K)
+{
+    __shared__ float tileA[TILE_SIZE][TILE_SIZE];
+    __shared__ float tileB[TILE_SIZE][TILE_SIZE];
+
+    int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+    int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+
+    float sum = 0.0f;
+
+    // A = MxK, B = KxN, C = MxN
+    // M = number of rows in A and C
+    // N = number of columns in B and C
+    // K = number of columns in A and rows in B
+    
+    for (int t = 0; t < (K + TILE_SIZE - 1) / TILE_SIZE; ++t) {
+        
+        if (row < M && t*TILE_SIZE + threadIdx.x < K)
+            tileA[threadIdx.y][threadIdx.x] = A[row * K + t * TILE_SIZE + threadIdx.x];
+        else
+            tileA[threadIdx.y][threadIdx.x] = 0.0f;
+
+        if (t * TILE_SIZE + threadIdx.y < K && col < N) 
+            tileB[threadIdx.y][threadIdx.x] = B[(t * TILE_SIZE + threadIdx.y) * N + col];
+        else
+            tileB[threadIdx.y][threadIdx.x] = 0.0f;
+
+        __syncthreads();
+
+        for (int i = 0; i < TILE_SIZE; ++i) {
+            sum += tileA[threadIdx.y][i] * tileB[i][threadIdx.x];
+        }
+
+        __syncthreads();
+    }
+
+    if (row < M && col < N) {
+        C[row * N + col] = sum;
+    }
+}
+
+int main()
+{
+    int M = 2, K = 3, N = 1;
+
+    float A[M * K] = {
+        1, 2, 3,
+        4, 5, 6,
+    };
+    float B[K * N] = {
+        9,
+        8,
+        7
+    };
+    float C[M * N] = {0};
+
+    float *d_A, *d_B, *d_C;
+
+    cudaMalloc(&d_A, M * K * sizeof(float));
+    cudaMalloc(&d_B, K * N * sizeof(float));
+    cudaMalloc(&d_C, M * N * sizeof(float));
+
+    cudaMemcpy(d_A, A, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, K * N * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 blockSize(TILE_SIZE, TILE_SIZE);
+    dim3 gridSize((N + TILE_SIZE - 1) / TILE_SIZE, (M + TILE_SIZE - 1) / TILE_SIZE);
+
+    MatMul2D<<<gridSize, blockSize>>>(d_A, d_B, d_C, M, N, K);
+
+    cudaMemcpy(C, d_C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    // result should be C = [ 46, 118 ]
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            printf("%f ", C[i * N + j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
